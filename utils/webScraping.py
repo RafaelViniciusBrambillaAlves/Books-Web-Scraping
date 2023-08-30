858a11fc235fb4eabcd42273afa4eaffcfbacfff
Rafael Vinicius Brambilla Alves <128416211+RafaelViniciusBrambillaAlves@users.noreply.github.com>
2023-08-30 10:24:21 -0300

    Update webScraping.py

--- utils/webScraping.py
+++ utils/webScraping.py
@@ -22,7 +22,7 @@ def web_Scraping():
 
     #My User Agent no Google
     headers =   {
-                'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+                'User-Agent': ""
                 }
 
     #Definindo a URL base do site
